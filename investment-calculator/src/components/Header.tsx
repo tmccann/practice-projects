458d16581ed9045cc838c384aa9fb06399bb0315
Tommy <tommy.mccann001@gmail.com>
2025-05-26 15:58:44 +0100

    Add test file with outlined header tests and comments

--- investment-calculator/src/components/Header.tsx
+++ investment-calculator/src/components/Header.tsx
@@ -1,5 +1,12 @@
+import logo from "../assets/investment-calculator-logo.png";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <img src={logo} alt="Money bag surrounded by gold coins stacks" />
+      <h1>Investment Calculator</h1>
+    </header>
+  );
 };
 
 export default Header;
--- investment-calculator/src/tests/__tests__/header.test.tsx
+++ investment-calculator/src/tests/__tests__/header.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Header from "../../components/Header";
 
 describe("header component", () => {
@@ -7,11 +7,21 @@ describe("header component", () => {
   });
   test("renders header component", () => {
     // check if header exist in dom
+    const header = screen.getByRole("banner");
+    expect(header).toBeInTheDocument();
   });
   test("renders header with correct text", () => {
     // check that the text "investment Calculator"
+    const title = screen.getByRole("heading", {
+      name: "Investment Calculator",
+    });
+    expect(title).toBeInTheDocument();
   });
   test("renders image with correct alt text", () => {
     // / Check for the <img> with the alt text "Money bag surrounded by gold coin stacks ".
+    const logo = screen.getByAltText(
+      "Money bag surrounded by gold coins stacks"
+    );
+    expect(logo).toBeInTheDocument();
   });
 });
