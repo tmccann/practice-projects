c304842c105a73f93a411a5617398bb0f6f3036c
Tommy <tommy.mccann001@gmail.com>
2025-05-27 16:28:19 +0100

    feat(Result): implement table header to satisfy existing tests

--- investment-calculator/src/components/Result.tsx
+++ investment-calculator/src/components/Result.tsx
@@ -1,7 +1,17 @@
-import React from "react";
-
 const Result = () => {
-  return <div>Result</div>;
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest(Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+    </table>
+  );
 };
 
 export default Result;
