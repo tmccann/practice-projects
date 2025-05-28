4d97bf53ca6eb300d0a0d6591fa285d292161fc5
Tommy <tommy.mccann001@gmail.com>
2025-05-28 12:40:11 +0100

    Update tests after lifting form state

--- investment-calculator/src/tests/__tests__/InvestmentForm.test.tsx
+++ investment-calculator/src/tests/__tests__/InvestmentForm.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import InvestmentForm from "../../components/InvestmentForm";
 import userEvent from "@testing-library/user-event";
 import InvestmentFormWrapper from "./testWrappers/InvestmentFormWrapper";
 
