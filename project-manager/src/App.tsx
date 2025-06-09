596a005cba0dbefeaf27f87b78cae05543ac7112
Tommy <tommy.mccann001@gmail.com>
2025-06-09 08:59:10 +0100

    chore: add pseudocode for layout and project/task logic

--- project-manager/src/App.tsx
+++ project-manager/src/App.tsx
@@ -1,7 +1,13 @@
 export default function App() {
   return (
     <>
-    
+      {/* Layout: container split into 30% sidebar, 70% main content */}
+
+      {/* Sidebar: shows list of projects to select from */}
+
+      {/* Main content:
+          - Show "Add Project" form when no project is selected
+          - Show list of tasks for selected project otherwise */}
     </>
   );
 }
