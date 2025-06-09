84f924e59cedf1c0917a06545474981520a38aa9
Tommy <tommy.mccann001@gmail.com>
2025-06-09 10:32:43 +0100

    feat(sidebar): add sidebar markup to satisfy render tests; onClick test remains failing

--- project-manager/src/components/SideBar.tsx
+++ project-manager/src/components/SideBar.tsx
@@ -1,5 +1,16 @@
 const SideBar = () => {
-  return <div>SideBar</div>;
+  const handleNewProject = () => {};
+  return (
+    <aside className="w-1/3 px-8 py-16 bg-stone-900  md:w-72 rounded-tr-2xl">
+      <h2 className="mb-8 text-stone-200 text-xl font-bold uppercase ">
+        your projects
+      </h2>
+
+      <button className="bg-stone-700 text-stone-400  hover:bg-stone-600  hover:text-stone-100 py-2 px-4 rounded-md ">
+        + Add Project
+      </button>
+    </aside>
+  );
 };
 
 export default SideBar;
