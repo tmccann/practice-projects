e0a2e87672dcb45a8a426d695a11f166852aec6d
Tommy <tommy.mccann001@gmail.com>
2025-06-09 10:03:17 +0100

    test: add placeholder sidebar component and initial failing tests

--- project-manager/src/tests/sidebar.test.tsx
+++ project-manager/src/tests/sidebar.test.tsx
@@ -1,6 +1,6 @@
 import { screen, render } from "@testing-library/react";
 import SideBar from "../components/SideBar";
-
+import userEvent from "@testing-library/user-event";
 const getSideBarElements = () => {
   return {
     sidebar: screen.getByRole("complementary"),
@@ -9,10 +9,22 @@ const getSideBarElements = () => {
   };
 };
 
-describe("sideBar component", () => {
-  beforeEach(() => {
-    render(<SideBar />);
-  });
-  test("aside, h2, and button should be rendered, with correct content", () => {});
-  test("button should fire handleNewProject on click", () => {});
+const user = userEvent.setup();
+
+test("aside, h2, and button should be rendered, with correct content", () => {
+  render(<SideBar />);
+  const { sidebar, h2, button } = getSideBarElements();
+  expect(sidebar).toBeInTheDocument();
+  expect(h2).toBeInTheDocument();
+  expect(button).toBeInTheDocument();
+  expect(h2).toHaveTextContent("your projects");
+  expect(button).toHaveTextContent("+ Add Project");
+});
+test("button should fire handleNewProject on click", () => {
+  const handleNewProject = vi.fn(); // or jest.fn() if using Jest
+
+  render(<SideBar handleNewProject={handleNewProject} />);
+  const { button } = getSideBarElements();
+  user.click(button);
+  expect(handleNewProject).toHaveBeenCalled();
 });
